582ec385872711422ee2e000ba6355d8c4683955
Przemysław <pp406298@students.mimuw.edu.pl>
2020-11-22 17:04:14 +0100

    working COO representation that save/load data

--- coordinate_list/coordinate_list.cpp
+++ coordinate_list/coordinate_list.cpp
@@ -17,19 +17,26 @@ template<typename T>
 class COO : public multiplicator<T> {
     const std::string _namespace = "zpp";
     const std::string _table_name = "coo_test_matrix";
-    const int _block_size = 32;
+    const int _block_size = 3;
     std::shared_ptr<connector> _conn;
     size_t _matrix_id;
     size_t result_id = 100;
 
     void submit_block(std::vector<matrix_value<T>>& block, size_t block_id, size_t matrix_id) {
+        if (block.empty()) return;
+
         requestor query(_conn);
-        query << "INSERT INTO zpp.vectors (block_id, matrix_id, vals) "
+        query << "INSERT INTO " << _namespace << "." << _table_name << " (block_id, matrix_id, vals) "
                  "   VALUES (" << block_id  << ", " << matrix_id << ", {";
+
+        auto last = block.back();
+        block.pop_back();
+
         for (auto value : block) {
             query << "(" << value.i << ", " << value.j << ", " << value.val << "), ";
         }
-        query << "});";
+        query << "(" << last.i << ", " << last.j << ", " << last.val << ")});";
+
         query.send();
     }
 
@@ -76,8 +83,8 @@ class COO : public multiplicator<T> {
 
     size_t get_block_index_for_cell(std::pair<size_t, size_t> pos) {
         size_t first = div_up(pos.first, _block_size);
-        size_t second = div_up(pos.first, _block_size);
-        return div_up(dimension, _block_size) * first + second;
+        size_t second = div_up(pos.second, _block_size);
+        return div_up(dimension, _block_size) * (first - 1) + second;
     }
 public:
     COO(std::shared_ptr<connector> conn) : _conn(conn), _matrix_id(0) {
@@ -90,13 +97,17 @@ public:
                            "};";
         namespace_query.send();
 
+        requestor table_erase(_conn);
+        table_erase << "DROP TABLE " << _namespace << "." << _table_name << ";";
+        table_erase.send();
+
         requestor table_query(_conn);
         table_query << "CREATE TABLE " << _namespace << "." << _table_name << " ("
                        "    block_id int, "
                        "    matrix_id int, "
-                       "    vals set<tuple<int, int, double>>, "
+                       "    vals set<frozen<tuple<int, int, double>>>, "
                        "    PRIMARY KEY (block_id, matrix_id) "
-                       ") WITH CLUSTERING ORDER BY (coord ASC);";
+                       ") WITH CLUSTERING ORDER BY (matrix_id ASC);";
         table_query.send();
     }
 
@@ -105,19 +116,30 @@ public:
 
         int next_batch_start = _block_size + 1;
         size_t block_id = 0;
-        std::vector< std::vector<matrix_value<T>> > blocks;
+        std::vector< std::vector<matrix_value<T>> > blocks(div_up(dimension, _block_size));
+
+        std::cerr << gen.has_next() << std::endl;
 
         while (gen.has_next()) {
             matrix_value<T> next = gen.next();
             next.i++; next.j++; // generator generates fields from 0 to dimension-1
-            if (next.i >= next_batch_start) {
+            std::cout << next.i << " " << next.j << " " << gen.has_next() << std::endl;
+            while (next.i >= next_batch_start) {
                 for (auto &b : blocks) {
                     submit_block(b, ++block_id, _matrix_id);
                     b.clear();
                 }
+
+                next_batch_start += _block_size;
             }
 
-            next_batch_start += _block_size;
+            blocks[div_up(next.j, _block_size) - 1].push_back(next);
+        }
+
+        /* Submit the remainder */
+        for (auto &b : blocks) {
+            submit_block(b, ++block_id, _matrix_id);
+            b.clear();
         }
     }
 
@@ -128,7 +150,7 @@ public:
 
         for (size_t i = 1; i <= blocks_high; i++) {
             for (size_t j = 1; j <= blocks_wide; j++) {
-                size_t block_id = i * blocks_wide + j;
+                size_t block_id = (i - 1) * blocks_wide + j;
                 auto copy_from_a = get_block(block_id, _matrix_id - 1);
                 submit_block(copy_from_a, block_id, result_id);
             }
@@ -138,6 +160,7 @@ public:
     /* Obtains the value in the multiplication result at (x; y) = (pos.first; pos.second) */
     T get_result(std::pair<size_t, size_t> pos) {
         size_t block_id = get_block_index_for_cell(pos);
+        std::cerr << "Block for cell: " << pos.first << " " << pos.second << ": " << block_id << std::endl;
 
         auto target_block = get_block(block_id, result_id);
         for (auto& val : target_block) {
@@ -154,7 +177,7 @@ int main(int argc, char* argv[]) {
     std::shared_ptr<connector> conn;
 
     try {
-        conn = std::make_shared<connector>("127.17.0.2");
+        conn = std::make_shared<connector>("172.17.0.2");
         std::cout << "Connected" << std::endl;
     } catch (...) {
         std::cout << "Connection error" << std::endl;
@@ -164,13 +187,13 @@ int main(int argc, char* argv[]) {
     std::shared_ptr factory = std::make_shared<float_value_factory>(0.0, 100.0, 0);
     COO<float> multiplicator_instance(conn);
 
-    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(dimension, dimension, 100, 0, factory));
-    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(dimension, dimension, 80, 1, factory));
+    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(dimension, dimension, 3, 0, factory));
+    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(dimension, dimension, 3, 1, factory));
 
     multiplicator_instance.multiply();
 
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
+    for (int i = 1; i <= dimension; i++) {
+        for (int j = 1; j <= dimension; j++) {
             auto result = multiplicator_instance.get_result({i, j});
             if (result != 0) {
                 std::cout << "(" << i << ", " << j << "): "  << result << std::endl;
