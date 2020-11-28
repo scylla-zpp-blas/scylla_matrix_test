04eead3c80eb4c25c33b7393ae9487f4963e42c3
Rhantolq <robertos1727@gmail.com>
2020-11-28 17:22:11 +0100

    Revert "test"
    This reverts commit 9e56f5c5add802f729d7941f5694f355f61bfbcd.

--- dictionary_of_keys/dictionary_of_keys.cc
+++ dictionary_of_keys/dictionary_of_keys.cc
@@ -229,11 +229,9 @@ public:
                     }
                 }
 
-                if (_sum != 0) {
-                    std::vector<matrix_value<T>> _upload_block;
-                    _upload_block.emplace_back(_f_start->i, _s_start->i, _sum);
-                    submit_block(_upload_block, result_id);
-                }
+                std::vector<matrix_value<T>> _upload_block;
+                _upload_block.emplace_back(_f_start->i, _s_start->i, _sum);
+                submit_block(_upload_block, result_id);
 
                 _s_start = fetch_next_coords(_s_start->i + 1, 2);
             }
