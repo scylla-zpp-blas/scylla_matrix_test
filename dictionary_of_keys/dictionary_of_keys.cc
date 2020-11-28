3540d1cad019bf7bc5cab6afdc8cfda8c846d2e0
Rhantolq <robertos1727@gmail.com>
2020-11-28 13:57:03 +0100

    test

--- dictionary_of_keys/dictionary_of_keys.cc
+++ dictionary_of_keys/dictionary_of_keys.cc
@@ -5,170 +5,213 @@
 #include <memory>
 #include <string>
 #include <random>
+#include <optional>
 #include "../multiplicator.h"
 #include "../utils/connector.h"
 #include "../utils/requestor.h"
 #include "../float_value_factory.hh"
 #include "../sparse_matrix_value_generator.hh"
 
-static int dimension = 5;
-
 template<typename T>
-class COO : public multiplicator<T> {
-    const std::string _namespace = "zpp";
-    const std::string _table_name = "coo_test_matrix";
-    const int _block_size = 3;
+class dok : public multiplicator<T> {
+private:
+    const std::string _KEYSPACE_NAME = "zpp";
+    const std::string _TABLE_NAME = "dok_test_matrix";
+    const size_t _BLOCK_SIZE = 1000;
     std::shared_ptr<connector> _conn;
     size_t _matrix_id;
     size_t result_id = 100;
+    size_t _first_matrix_height, _first_matrix_width, _second_matrix_height, _second_matrix_width;
 
-    void submit_block(std::vector<matrix_value<T>>& block, size_t block_id, size_t matrix_id) {
+    void submit_block(const std::vector<matrix_value<T>>& block, size_t matrix_id) {
         if (block.empty()) return;
 
-        requestor query(_conn);
-        query << "INSERT INTO " << _namespace << "." << _table_name << " (block_id, matrix_id, vals) "
-                                                                       "   VALUES (" << block_id  << ", " << matrix_id << ", {";
-
-        auto last = block.back();
-        block.pop_back();
-
-        for (auto value : block) {
-            query << "(" << value.i << ", " << value.j << ", " << value.val << "), ";
+        for (auto val: block) {
+            requestor query(_conn);
+            query << "INSERT INTO " << _KEYSPACE_NAME << "." << _TABLE_NAME << " (matrix_id, pos_x, pos_y, val) "
+                                                                               "   VALUES ("
+                                                                               << matrix_id << ", "
+                                                                               << val.i << ", "
+                                                                               << val.j << ", "
+                                                                               << val.val << ");";
+            query.send();
         }
-        query << "(" << last.i << ", " << last.j << ", " << last.val << ")});";
-
-        query.send();
     }
 
-    std::vector<matrix_value<T>> get_block(size_t block_id, size_t matrix_id) {
+    inline std::optional<matrix_value<T>> fetch_next_coords(size_t min_pos_x, size_t min_pos_y, size_t matrix_id) {
         requestor query(_conn);
-        query << "SELECT * FROM " << _namespace << "." << _table_name
-              << " WHERE block_id=" << block_id << " AND matrix_id=" << matrix_id << ";";
+        query << "SELECT * FROM " << _KEYSPACE_NAME << "." << _TABLE_NAME
+              << " WHERE pos_x>=" << min_pos_y << " AND pos_y>=" << min_pos_y << " AND matrix_id=" << matrix_id
+              << " LIMIT " << 1 << ";";
         query.send();
 
-        std::vector<matrix_value<float>> ret;
-
         if (query.next_row()) {
-            const CassValue* value_set = query.get_column("vals");
-            CassIterator* set_iterator = cass_iterator_from_collection(value_set);
-            while (cass_iterator_next(set_iterator)) {
-                const CassValue *value_i, *value_j, *value_val;
-                int i, j; double val;
+            const CassValue* _cass_pos_x = query.get_column("pos_x");
+            const CassValue* _cass_pos_y = query.get_column("pos_y");
+            const CassValue* _cass_val = query.get_column("val");
+            cass_int64_t _pos_x, _pos_y;
+            cass_double_t _val;
 
-                const CassValue* value_tuple = cass_iterator_get_value(set_iterator);
-                CassIterator* tuple_iterator = cass_iterator_from_tuple(value_tuple);
+            cass_value_get_int64(_cass_pos_x, &_pos_x);
+            cass_value_get_int64(_cass_pos_y, &_pos_y);
+            cass_value_get_double(_cass_val, &_val);
 
-                cass_iterator_next(tuple_iterator);
-                cass_value_get_int32(cass_iterator_get_value(tuple_iterator), &i);
+            return std::make_optional(matrix_value<T>(_pos_x, _pos_y, _val));
+        }
 
-                cass_iterator_next(tuple_iterator);
-                cass_value_get_int32(cass_iterator_get_value(tuple_iterator), &j);
+        return std::nullopt;
+    }
 
-                cass_iterator_next(tuple_iterator);
-                cass_value_get_double(cass_iterator_get_value(tuple_iterator), &val);
+    inline std::vector<matrix_value<T>> get_block(size_t min_pos_x, size_t pos_y, size_t matrix_id) {
+        requestor query(_conn);
+        query << "SELECT * FROM " << _KEYSPACE_NAME << "." << _TABLE_NAME
+              << " WHERE pos_x>=" << min_pos_x << " AND pos_y=" << pos_y << " AND matrix_id=" << matrix_id
+              << " LIMIT" << _BLOCK_SIZE << ";";
+        query.send();
 
-                cass_iterator_free(tuple_iterator);
+        std::vector<matrix_value<T>> ret;
 
-                ret.emplace_back(i, j, val);
-            }
-            cass_iterator_free(set_iterator);
+        while (query.next_row()) {
+            const CassValue* _cass_pos_x = query.get_column("pos_x");
+            const CassValue* _cass_pos_y = query.get_column("pos_y");
+            const CassValue* _cass_val = query.get_column("val");
+            cass_int64_t _pos_x, _pos_y;
+            cass_double_t _val;
+
+            cass_value_get_int64(_cass_pos_x, &_pos_x);
+            cass_value_get_int64(_cass_pos_y, &_pos_y);
+            cass_value_get_double(_cass_val, &_val);
+
+            ret.emplace_back(_pos_x, _pos_y, _val);
         }
 
         return ret;
     }
 
-    size_t div_up(size_t val, size_t divisor) {
-        return (val - 1) / divisor + 1;
-    }
-
-    size_t get_block_index_for_cell(std::pair<size_t, size_t> pos) {
-        size_t first = div_up(pos.first, _block_size);
-        size_t second = div_up(pos.second, _block_size);
-        return div_up(dimension, _block_size) * (first - 1) + second;
-    }
 public:
-    COO(std::shared_ptr<connector> conn) : _conn(conn), _matrix_id(0) {
+    explicit dok(std::shared_ptr<connector> conn) : _conn(conn), _matrix_id(0) {
         /* Make sure that the necessary namespaces and table exist */
 
         requestor namespace_query(_conn);
-        namespace_query << "CREATE KEYSPACE IF NOT EXISTS " << _namespace << " WITH REPLICATION = {"
-                                                                             "    'class' : 'SimpleStrategy',"
-                                                                             "    'replication_factor' : 1"
-                                                                             "};";
+        namespace_query << "CREATE KEYSPACE IF NOT EXISTS " << _KEYSPACE_NAME << " WITH REPLICATION = {"
+                                                                                 "    'class' : 'SimpleStrategy',"
+                                                                                 "    'replication_factor' : 1"
+                                                                                 "};";
         namespace_query.send();
 
         requestor table_erase(_conn);
-        table_erase << "DROP TABLE " << _namespace << "." << _table_name << ";";
+        table_erase << "DROP TABLE " << _KEYSPACE_NAME << "." << _TABLE_NAME << ";";
         table_erase.send();
 
         requestor table_query(_conn);
-        table_query << "CREATE TABLE " << _namespace << "." << _table_name << " ("
-                                                                              "    block_id int, "
-                                                                              "    matrix_id int, "
-                                                                              "    vals set<frozen<tuple<int, int, double>>>, "
-                                                                              "    PRIMARY KEY (block_id, matrix_id) "
-                                                                              ") WITH CLUSTERING ORDER BY (matrix_id ASC);";
+        table_query << "CREATE TABLE " << _KEYSPACE_NAME << "." << _TABLE_NAME << " ("
+                                                                                  "    matrix_id int, "
+                                                                                  "    pos_x bigint, "
+                                                                                  "    pos_y bigint, "
+                                                                                  "    val double, "
+                                                                                  "    PRIMARY KEY (matrix_id, pos_x, pos_y) "
+                                                                                  ") WITH CLUSTERING ORDER BY (pos_y ASC, pos_x ASC);";
         table_query.send();
     }
 
-    void load_matrix(matrix_value_generator<T>&& gen) {
+    void load_matrix(matrix_value_generator<T>&& gen) override {
         _matrix_id++;
 
-        int next_batch_start = _block_size + 1;
-        size_t block_id = 0;
-        std::vector< std::vector<matrix_value<T>> > blocks(div_up(dimension, _block_size));
+        bool transpose;
 
-        std::cerr << gen.has_next() << std::endl;
-
-        while (gen.has_next()) {
-            matrix_value<T> next = gen.next();
-            next.i++; next.j++; // generator generates fields from 0 to dimension-1
-            std::cout << next.i << " " << next.j << " " << gen.has_next() << std::endl;
-            while (next.i >= next_batch_start) {
-                for (auto &b : blocks) {
-                    submit_block(b, ++block_id, _matrix_id);
-                    b.clear();
-                }
-
-                next_batch_start += _block_size;
-            }
-
-            blocks[div_up(next.j, _block_size) - 1].push_back(next);
+        if (_matrix_id == 1) {
+            _first_matrix_height = gen.height();
+            _first_matrix_width = gen.width();
+            transpose = false;
+        }
+        else {
+            _second_matrix_height = gen.width();
+            _second_matrix_width = gen.height();
+            transpose = true;
         }
 
-        /* Submit the remainder */
-        for (auto &b : blocks) {
-            submit_block(b, ++block_id, _matrix_id);
-            b.clear();
+        std::vector<matrix_value<T>> _block;
+
+        while (gen.has_next()) {
+            matrix_value<T> _next = gen.next();
+            std::cout << _next.i << " " << _next.j << " " << gen.has_next() << std::endl;
+
+            if (transpose) {
+                std::swap(_next.i, _next.j);
+            }
+
+            _block.push_back(_next);
+
+            if (_block.size() >= _BLOCK_SIZE) {
+                submit_block(_block, _matrix_id);
+                _block.clear();
+            }
+
         }
     }
 
     /* Multiplies two matrices loaded into Scylla with load_matrix. */
-    void multiply() {
-        size_t blocks_wide = div_up(dimension, _block_size);
-        size_t blocks_high = div_up(dimension, _block_size);
+    void multiply() override {
+        size_t _width = std::min(_first_matrix_width, _second_matrix_width);
+        size_t _height = std::min(_first_matrix_height, _second_matrix_height);
 
-        for (size_t i = 1; i <= blocks_high; i++) {
-            for (size_t j = 1; j <= blocks_wide; j++) {
-                size_t block_id = (i - 1) * blocks_wide + j;
-                auto copy_from_a = get_block(block_id, _matrix_id - 1);
-                submit_block(copy_from_a, block_id, result_id);
+        // Find position of first non-zero row of first matrix.
+        auto _f_start = fetch_next_coords(0, 0, 1);
+        while (_f_start.has_value()) {
+            // Find position of first non-zero transposed column of second matrix.
+            auto _s_start = fetch_next_coords(_f_start->j, 0, 2);
+            while (_s_start.has_value()) {
+                // Multiply row by transposed column.
+                auto _f_block = get_block(_f_start->j, _f_start->i, 1);
+                auto _s_block = get_block(_s_start->j, _s_start->i, 2);
+                T _sum = 0;
+                size_t i = 0, j = 0;
+
+                while (true) {
+                    while (i < _f_block.size() && _f_block[i].j <= _s_block[j].j) {
+                        if (_f_block[i].j == _s_block[i].j) {
+                            _sum += _f_block[i].val * _s_block[i].val;
+                        }
+                        i++;
+                    }
+                    if (i == _f_block.size()) {
+                        _f_block = get_block(_f_block[i - 1].j, _f_start->i, 1);
+                        i = 0;
+                        if (_f_block.empty()) {
+                            break;
+                        }
+                    }
+                    while (j < _s_block.size() && _s_block[j].j <= _f_block[i].j) {
+                        if (_s_block[j].j == _f_block[i].j) {
+                            _sum += _s_block[j].val * _f_block[i].val;
+                        }
+                        j++;
+                    }
+                    if (j == _s_block.size()) {
+                        _s_block = get_block(_s_block[j - 1].j, _s_start->i, 2);
+                        j = 0;
+                        if (_s_block.empty()) {
+                            break;
+                        }
+                    }
+                }
+
+                std::vector<matrix_value<T>> _upload_block;
+                _upload_block.emplace_back(_f_start->i, _s_start->i, _sum);
+                submit_block(_upload_block, result_id);
+
+                _s_start = fetch_next_coords(_f_start->j, _s_start->i + 1, 2);
             }
+            _f_start = fetch_next_coords(0, _f_start->i + 1, 1);
         }
     }
 
     /* Obtains the value in the multiplication result at (x; y) = (pos.first; pos.second) */
-    T get_result(std::pair<size_t, size_t> pos) {
-        size_t block_id = get_block_index_for_cell(pos);
-        std::cerr << "Block for cell: " << pos.first << " " << pos.second << ": " << block_id << std::endl;
-
-        auto target_block = get_block(block_id, result_id);
-        for (auto& val : target_block) {
-            if (val.i == pos.first && val.j == pos.second) {
-                return val.val;
-            }
+    T get_result(std::pair<size_t, size_t> pos) override {
+        auto result = fetch_next_coords(pos.first, pos.second, result_id);
+        if (!result.has_value() && result->j == pos.first && result->i == pos.second) {
+            return result->val;
         }
-
         return 0;
     }
 };
@@ -177,7 +220,7 @@ int main(int argc, char* argv[]) {
     std::shared_ptr<connector> conn;
 
     try {
-        conn = std::make_shared<connector>("172.17.0.2");
+        conn = std::make_shared<connector>("192.168.1.146");
         std::cout << "Connected" << std::endl;
     } catch (...) {
         std::cout << "Connection error" << std::endl;
@@ -185,15 +228,16 @@ int main(int argc, char* argv[]) {
     }
 
     std::shared_ptr factory = std::make_shared<float_value_factory>(0.0, 100.0, 0);
-    COO<float> multiplicator_instance(conn);
+    dok<float> multiplicator_instance(conn);
 
-    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(dimension, dimension, 3, 0, factory));
-    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(dimension, dimension, 3, 1, factory));
+    size_t DIMENSION = 3;
+    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(DIMENSION, DIMENSION, 3, 0, factory));
+    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(DIMENSION, DIMENSION, 3, 1, factory));
 
     multiplicator_instance.multiply();
 
-    for (int i = 1; i <= dimension; i++) {
-        for (int j = 1; j <= dimension; j++) {
+    for (int i = 1; i <= DIMENSION; i++) {
+        for (int j = 1; j <= DIMENSION; j++) {
             auto result = multiplicator_instance.get_result({i, j});
             if (result != 0) {
                 std::cout << "(" << i << ", " << j << "): "  << result << std::endl;
