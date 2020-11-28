32bcd7a2526d01d3d9f6588b4c822e857dcb9366
Rhantolq <robertos1727@gmail.com>
2020-11-28 16:09:36 +0100

    etr3e45

--- dictionary_of_keys/dictionary_of_keys.cc
+++ dictionary_of_keys/dictionary_of_keys.cc
@@ -262,8 +262,8 @@ int main(int argc, char* argv[]) {
     dok<float> multiplicator_instance(conn);
 
     size_t DIMENSION = 4;
-    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(DIMENSION, DIMENSION, 5, 0, factory));
-    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(DIMENSION, DIMENSION, 5, 1, factory));
+    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(DIMENSION, DIMENSION, 5, 2137, factory));
+    multiplicator_instance.load_matrix(sparse_matrix_value_generator<float>(DIMENSION, DIMENSION, 5, 2137, factory));
 
     multiplicator_instance.multiply();
 
